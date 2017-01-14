0187e0e1a80c0d64c7eb86a4f428426b0629b6ae
Jan Hjørdie <jan@hjordie.dk>
2017-01-14 13:37:58 +0100

    1 commit

---
+++ events4singlerPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace events4singler
+{
+	public partial class events4singlerPage : ContentPage
+	{
+		public events4singlerPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
